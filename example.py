cd7f2ef4a6683528ab490c0edc2f78193f8580ee
Hansimov <591172499@qq.com>
2024-09-16 04:10:16 +0800

    :pencil: [Doc] Readme: update example script with new feature, and upgrade to v1.0.2

--- example.py
+++ example.py
@@ -12,8 +12,10 @@ from tclogger import (
     str_to_ts,
     get_now_ts_str,
     Runtimer,
+    dict_to_str,
 )
 from tclogger import OSEnver, shell_cmd
+from rich.console import Console
 
 if __name__ == "__main__":
     with Runtimer():
@@ -21,5 +23,14 @@ if __name__ == "__main__":
         logger.mesg(get_now_ts())
         logger.success(get_now_str())
         logger.note(f"Now: {logstr.mesg(get_now_str())}, ({logstr.file(get_now_ts())})")
+
+    d = {
+        "hello": "world",
+        "now": get_now_str(),
+        "list": [1, 2, 3, [4, 5], "6"],
+        "nested": {"key1": "value1", "key2": "value2", "key_3": {"subkey": "subvalue"}},
+    }
+    s = dict_to_str(d, max_depth=1)
+    print(s)
 
     # python example.py
