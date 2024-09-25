bfe2e0272c1b26495a14299cf0e4ab299cbe6a98
Hansimov <591172499@qq.com>
2024-09-25 10:52:15 +0800

    :zap: [Enhance] maths: more smart and correct algo for int_bits

--- src/tclogger/maths.py
+++ src/tclogger/maths.py
@@ -5,9 +5,7 @@ import math
 
 # calculate bits of integer in base
 def int_bits(num, base: int = 10):
-    if num == 0:
-        return 0
-    return int(math.log(num, base) + 1)
+    return math.ceil(math.log(num + 1, base))
 
 
 # get max length of dict keys
