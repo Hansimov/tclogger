87027fcf9ae93b7b6a1c4d65a330fd2c99c44ac0
Hansimov <591172499@qq.com>
2024-10-04 20:53:10 +0800

    :zap: [Enhance] CursorController: use abs to fix row<0, separate write_and_flush, and not flush to avoid glitch

--- src/tclogger/cursors.py
+++ src/tclogger/cursors.py
@@ -8,7 +8,17 @@ class CursorController:
     - https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
     """
 
-    def move(self, row: int = 0, col: int = 0, write: bool = True):
+    def __init__(self, write: bool = True, flush: bool = False):
+        self.write = write
+        self.flush = flush
+
+    def write_and_flush(self, move_str: str):
+        if self.write and move_str:
+            sys.stdout.write(move_str)
+            if self.flush:
+                sys.stdout.flush()
+
+    def move(self, row: int = 0, col: int = 0):
         """Move cursor to the relative position:
         - row > 0: rows up
         - row < 0: rows down
@@ -20,6 +30,7 @@ class CursorController:
         if row > 0:
             move_str = f"\033[{row}A"  # move cursor up by row
         elif row < 0:
+            row = abs(row)
             move_str = f"\033[{row}B"  # move cursor down by row
         else:
             move_str = ""
@@ -27,17 +38,15 @@ class CursorController:
         if col > 0:
             move_str += f"\033[{col}C"  # move cursor right by col
         elif col < 0:
+            col = abs(col)
             move_str += f"\033[{col}D"  # move cursor left by col
         else:
             pass
 
-        if write and move_str:
-            sys.stdout.write(move_str)
-            sys.stdout.flush()
-
+        self.write_and_flush(move_str)
         return move_str
 
-    def move_to_beg(self, row: int = 0, write: bool = True):
+    def move_to_beg(self, row: int = 0):
         """Move cursor to the beginning of the relative row:
         - row > 0: rows up
         - row < 0: rows down
@@ -46,33 +55,27 @@ class CursorController:
         if row > 0:
             move_str = f"\033[{row}F"  # move cursor to line beginning of rows above
         elif row < 0:
+            row = abs(row)
             move_str = f"\033[{row}E"  # move cursor to line beginning of rows below
         else:
             move_str = f"\033[1G"  # move cursor to line beginning of current row
 
-        if write and move_str:
-            sys.stdout.write(move_str)
-            sys.stdout.flush()
-
+        self.write_and_flush(move_str)
         return move_str
 
-    def move_to_col(self, col: int = 0, write: bool = True):
+    def move_to_col(self, col: int = 0):
         """Move cursor to column of current line."""
         if col > 0:
             move_str = f"\033[{col}G"  # move cursor to column of current line
         else:
             move_str = ""
 
-        if write and move_str:
-            sys.stdout.write(move_str)
-            sys.stdout.flush()
-
+        self.write_and_flush(move_str)
         return move_str
 
     def erase_line(
         self,
         part: Literal["cursor_to_end", "beg_to_cursor", "beg_to_end"] = "beg_to_end",
-        write: bool = True,
     ):
         """Erase the line:
         - cursor_to_end: erase from cursor to the end of the line
@@ -88,7 +91,5 @@ class CursorController:
         else:
             erase_str = ""
 
-        if write and erase_str:
-            sys.stdout.write(erase_str)
-            sys.stdout.flush()
+        self.write_and_flush(erase_str)
         return erase_str
